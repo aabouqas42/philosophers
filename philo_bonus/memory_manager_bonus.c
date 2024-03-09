0319a3f1f822402b27ca361867321b5574e37e91
aabouqas <Aissam520@gmail.com>
2024-03-09 17:10:47 +0100

    done

--- philo_bonus/memory_manager_bonus.c
+++ philo_bonus/memory_manager_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aabouqas <aabouqas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:29:25 by aabouqas          #+#    #+#             */
-/*   Updated: 2024/03/09 16:27:25 by aabouqas         ###   ########.fr       */
+/*   Updated: 2024/03/09 17:10:41 by aabouqas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,15 @@ void	_free(t_data *data)
 	char	*number;
 
 	sem_close(data->sem_forks);
+	sem_close(data->sem_printf);
+	sem_unlink("sem_printf");
+	sem_unlink("sem_forks");
 	i = 0;
 	while (i < data->numof_philos)
 	{
 		number = _itoa(i + 3);
+		if (number == NULL)
+			break ;
 		sem_close(data->sem_locks[i]);
 		sem_unlink(number);
 		free(number);
@@ -29,6 +34,19 @@ void	_free(t_data *data)
 	}
 	free (data->sem_locks);
 	free (data->ids);
+}
+
+sem_t	*open_semaphopre(t_data *data, char *name, int value)
+{
+	sem_t	*fd;
+
+	fd = sem_open(name, O_CREAT, 0666, value);
+	if (fd == SEM_FAILED)
+	{
+		_puts("Unexpected Error\n", 2);
+		exit(-1);
+	}
+	return (fd);
 }
 
 void	memory_init(t_data *data, char **argv)
@@ -41,7 +59,7 @@ void	memory_init(t_data *data, char **argv)
 	{
 		number = _itoa(i + 3);
 		if (number == NULL)
-			(_puts("Unexpected Error\n", 2), exit(0));
+			(_puts("Unexpected Error\n", 2), exit(-1));
 		sem_unlink(number);
 		free(number);
 		i++;
@@ -49,8 +67,8 @@ void	memory_init(t_data *data, char **argv)
 	sem_unlink("sem_printf");
 	sem_unlink("sem_forks");
 	data->numof_philos = _atoi(argv[1]);
-	data->sem_printf = sem_open("sem_printf", O_CREAT, 0666, 1);
-	data->sem_forks = sem_open("sem_forks", O_CREAT, 0666, data->numof_philos);
+	data->sem_printf = open_semaphopre(data, "sem_printf", 1);
+	data->sem_forks = open_semaphopre(data, "sem_forks", 1);
 	data->ids = malloc (data->numof_philos * sizeof(int));
 	if (data->ids == NULL)
 		(_puts("Unexpected Error\n", 2), exit(-1));
